d2eae09813a55c570c989855fdde2f201148f97f
OlenaKuchma <125207379+OlenaKuchma@users.noreply.github.com>
2023-06-17 19:18:15 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controlsDiv = document.querySelector('#controls');
+const createButton = controlsDiv.querySelector('[data-create]');
+const destroyButton = controlsDiv.querySelector('[data-destroy]');
+const boxesDiv = document.querySelector('#boxes');
+
+createButton.addEventListener('click', createBoxes);
+destroyButton.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = +controlsDiv.querySelector('input').value;
+  const boxes = [];
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    const size = 30 + i * 10;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+
+  boxesDiv.append(...boxes);
+}
+
+function destroyBoxes() {
+  boxesDiv.innerHTML = '';
+}
