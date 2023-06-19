1be6d3b019641bcb290d0cb578b19f3f3b9d6e41
OlenaKuchma <125207379+OlenaKuchma@users.noreply.github.com>
2023-06-19 19:59:49 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -3,9 +3,14 @@ const selectors = {
 input: document.querySelector('#name-input'),
 span: document.querySelector('#name-output')
 }
-selectors.input.addEventListener('change', handlerQuery)
 
-function handlerQuery(evt){
-    console.log(evt.currentTarget.value);
-    selectors.span.textContent = evt.currentTarget.value
-}
+
+selectors.input.addEventListener("input", (event) => {
+    
+    if (selectors.span.textContent = !event.currentTarget.value) {
+        selectors.span.textContent = "Anonymous!";
+    } else {
+        selectors.span.textContent = event.currentTarget.value;
+    }
+    
+  });
