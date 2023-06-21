1d2c54c7c4a52e2c8906287e51df34a594a56b7b
OlenaKuchma <125207379+OlenaKuchma@users.noreply.github.com>
2023-06-21 21:00:09 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,9 @@
 const selectors = {
     fontSizeControl: document.querySelector('#font-size-control'),
-textElement: document.querySelector('#text')};
-selectors.textElement.style.fontSize = selectors.fontSizeControl.value;
+    textElement: document.querySelector('#text')};
+
+selectors.textElement.style.fontSize = `${selectors.fontSizeControl.value}px`;
+
 selectors.fontSizeControl.addEventListener('input', () => {
   const fontSize = selectors.fontSizeControl.value;
   selectors.textElement.style.fontSize = `${fontSize}px`;
