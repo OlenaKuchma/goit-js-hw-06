ddb4317a5d1f87a7acb23daf3b105c5692882e83
OlenaKuchma <125207379+OlenaKuchma@users.noreply.github.com>
2023-06-21 20:52:59 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -4,13 +4,12 @@ input: document.querySelector('#name-input'),
 span: document.querySelector('#name-output')
 }
 
-
 selectors.input.addEventListener("input", (event) => {
-    
-    if (selectors.span.textContent = !event.currentTarget.value) {
-        selectors.span.textContent = "Anonymous";
+    if (event.currentTarget.value.trim().length === 0) {
+      selectors.span.textContent = "Anonymous";
     } else {
-        selectors.span.textContent = event.currentTarget.value;
+      selectors.span.textContent = event.currentTarget.value;
     }
-    
   });
+
+
